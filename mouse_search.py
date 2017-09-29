3d98b5e615ddbaab5a86e0b1633efa5435bb84bd
Fabian Schierok <fabian.schierok@tu-dortmund.de>
2017-09-29 19:41:37 +0200

    Add beep after reading mouse pos

--- mouse_search.py
+++ mouse_search.py
@@ -1,10 +1,15 @@
 from pynput.mouse import Controller
 from time import sleep
+import winsound
+Freq = 2500
+Dur = 500
 Mouse = Controller()
 sleep(10)
 x1, y1 = Mouse.position()
+winsound.Beep(Freq, Dur)
 sleep(3)
 x2, y2 = Mouse.position()
+winsound.Beep(Freq, Dur)
 with open("pos.txt", "w") as f:
     f.write(str(x1) + "   " + str(y1) + "\n")
     f.write(str(x2) + "   " + str(y2))
