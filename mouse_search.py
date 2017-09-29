025d75239bbd89fa941f50cae25d82a5e53e56b6
Fabian Schierok <fabian.schierok@tu-dortmund.de>
2017-09-29 19:58:36 +0200

    Fix error "Tupel not callable"

--- mouse_search.py
+++ mouse_search.py
@@ -5,10 +5,10 @@ Freq = 2500
 Dur = 500
 Mouse = Controller()
 sleep(10)
-x1, y1 = Mouse.position()
+x1, y1 = Mouse.position
 winsound.Beep(Freq, Dur)
 sleep(3)
-x2, y2 = Mouse.position()
+x2, y2 = Mouse.position
 winsound.Beep(Freq, Dur)
 with open("pos.txt", "w") as f:
     f.write(str(x1) + "   " + str(y1) + "\n")
